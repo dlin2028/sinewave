a8b3e0e33475175f212a3034f4a121f61869db1d
David Lin <David.Lin@GMR.local>
2019-01-18 17:34:48 -0800

    locked thad

--- SineWave/Form1.cs
+++ SineWave/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,6 +14,8 @@ namespace SineWave
 {
     public partial class Form1 : Form
     {
+        private static readonly object lockObject = new object();
+
         Bitmap map;
         Graphics gfx;
 
@@ -25,40 +28,54 @@ namespace SineWave
         double[][] inputs;
         double[][] outputs;
 
-        double scale = 1;
+        
+        int size = 100;
+        double range = Math.PI * 3;
+        double scale;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
             map = new Bitmap(ClientSize.Width, ClientSize.Height);
             gfx = Graphics.FromImage(map);
 
-            scale = ClientSize.Width / (Math.PI * 2);
+            scale = ClientSize.Width / range;
 
             int seed = Guid.NewGuid().GetHashCode();
             Random rand = new Random(seed);
 
             // 1, 100, 1
-            net = new NeuralNet(1, (100, Activations.Tanh));
+            net = new NeuralNet(1, (100, Activations.Tanh), (100, Activations.Tanh), (1, Activations.Tanh));
             net.Randomize(rand);
 
-            inputs = new double[ClientSize.Width][];
-            outputs = new double[ClientSize.Width][];
-            for (int i = 0; i < ClientSize.Width; i++)
-            {
-                double x = (double)i / ClientSize.Width * 2 * Math.PI;
-                double y = Math.Sin(x);
+            (inputs, outputs) = GenerateSine(size, range);
 
-                inputs[i] = new double[] { x };
-                outputs[i] = new double[] { y };
-            }
 
             backgroundWorker1.RunWorkerAsync();
         }
 
+        private (double[][] inputs, double[][] outputs) GenerateSine(int size, double range)
+        {
+            double[][] inputs = new double[size][];
+            double[][] outputs = new double[size][];
+
+            double x = 0;
+            for (int i = 0; i < size; i++)
+            {
+                inputs[i] = new double[] { x };
+                outputs[i] = new double[] { Math.Sin(x) };
+                x += range / size;
+            }
+
+            return (inputs, outputs);
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            var currentNet = net.Clone();
+            NeuralNet currentNet;
+            lock (lockObject)
+            {
+                currentNet = net.Clone();
+            }
             label1.Text = currentNet.MAE(inputs, outputs).ToString();
 
             
@@ -78,7 +95,10 @@ namespace SineWave
         {
             while(true)
             {
-                net.Backprop(inputs, outputs, 0.0005f);
+                lock (lockObject)
+                {
+                    net.Backprop(inputs, outputs, 0.0125, 0.2, 1);
+                }
             }
         }
     }
